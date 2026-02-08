ef386511733e96b9518ddf62e43a578efee6ed8d
sindri-afk <sindribjarka@gmail.com>
2026-02-08 15:39:14 +0000

    Update dateUtils.test.ts

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -6,3 +6,10 @@ describe("Date Utils", () => {
 });
 
 // functions to test
+// getCurrentYear() --> returns curr year
+//add(date, amount, type) --> adds time to a date
+// isWithinRange(date, from, to) --> checks if date is between two dates
+// isDateBefore(date, compareDate) --> checks if date is before another 
+// isSameDay(date, compareDate) --> checks if two dates are on the same day
+// getHolidays(year) --> Async; returns holiday dates for a year
+// isHoliday(date) --> Async; checks if date is a holiday
